d59485f51cc6561db4ca5f94efd6fc5f80947e53
unknown <abdelhedi.hlel@oyez.fr>
2019-01-08 13:51:11 +0100

    working with prisma methods

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,24 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'localhost:4466'
+  endpoint: 'http://localhost:4466'
 })
+const updatePostForUser = async (id, data) => {
+  const postExist = await prisma.exists.Post({
+    id
+  })
+  if (!postExist) throw new Error('Post does not exist')
+
+  const post = await prisma.mutation.updatePost({
+    data,
+    where: {
+      id
+    }
+  }, '{ author { id } }')
+  const user = await prisma.query.user({ where: { id: post.author.id } }, '{ id name }')
+  return user
+}
+
+updatePostForUser('cjqnm7pvu001c0795pk89yliy', { title: 'HAHAHAHHAHAH' })
+  .then(data => console.log('User Info', data))
+  .catch(err => console.log('eroor', err))
